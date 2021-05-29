80016f169b8929b8ee5f1e4cdeddf18d465a775d
Taku <63911366+saeki-taku@users.noreply.github.com>
2021-05-29 22:15:19 +0900

    404page make

--- 404.php
+++ 404.php
@@ -4,10 +4,10 @@
 
 	<main class="main">
 		<article class="l-common">
-			<img class="c-404__icon" src="<?= get_template_directory_uri(); ?>/images/404_icon.png" alt="傘を持った紳士" width="300" height="350">
-			<p class="c-404__text">恐れ入りますが該当のページは存在致しません。</p>
-			<div class="c-404__link-box">
-				<a class="c-404__link" href="<?= esc_url(home_url('/')); ?>">トップページへ</a>
+			<img class="p-404__icon" src="<?= get_template_directory_uri(); ?>/images/404_icon.png" alt="傘を持った紳士" width="300" height="350">
+			<p class="p-404__text">恐れ入りますが該当のページは存在致しません。</p>
+			<div class="p-404__link-box">
+				<a class="p-404__link" href="<?= esc_url(home_url('/')); ?>">トップページへ</a>
 			</div>
 		</article>
 	</main>
