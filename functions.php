bb3e052402fdc0a5e1b2964d034820b7c8b4644c
Taku <63911366+saeki-taku@users.noreply.github.com>
2021-05-31 13:40:09 +0900

    work hover-animation fix

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('init', function() {
+add_action('init', function () {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
   //メニューの追加
@@ -11,42 +11,48 @@ add_action('init', function() {
 });
 
 // head-title
-function wp_document_title_separator( $separator ) {
-  $separator = '|';
-  return $separator;
+function wp_document_title_separator($title)
+{
+  if (is_home() || is_front_page()) {
+    unset($title['tagline']); // ディスクリプションを出力しない
+  }
+  return $title;
 }
-add_filter( 'document_title_separator', 'wp_document_title_separator' );
+add_filter('document_title_parts', 'wp_document_title_separator', 10, 1);
+
 
 // front-pageの表示件数の設定
-function custom_loop_rules ( $query ) {
+function custom_loop_rules($query)
+{
   // 管理画面や、メインクエリ以外の処理に影響を及ぼさないように
-  if ( is_admin() || ! $query->is_main_query() ){
-      return;
+  if (is_admin() || !$query->is_main_query()) {
+    return;
   }
   // カテゴリーのアーカイブページ
-  if ( $query->is_home() ) {
-      $query->set('posts_per_page', 9); // 1ページあたりの表示件数を変更
+  if ($query->is_home()) {
+    $query->set('posts_per_page', 9); // 1ページあたりの表示件数を変更
   }
 }
-add_action('pre_get_posts','custom_loop_rules');
+add_action('pre_get_posts', 'custom_loop_rules');
 
 // MW WP Form エラーメッセージ
-function my_error_message($error, $key, $rule){
-    if($key === 'contact_name' && $rule === 'noempty'){
-        return 'お名前が入力されていません';
-    }
-    if($key === 'furigana' && $rule === 'noempty'){
-        return 'フリガナが入力されていません';
-    }
-    if($key === 'mail' && $rule === 'noempty'){
-        return 'E-mailが入力されていません';
-    }
-    if($key === 'tell' && $rule === 'noempty'){
-        return '電話番号が入力されていません';
-    }
-    if($key === 'contents' && $rule === 'noempty'){
-        return 'お問合せ内容が入力されていません';
-    }
-    return $error;
+function my_error_message($error, $key, $rule)
+{
+  if ($key === 'contact_name' && $rule === 'noempty') {
+    return 'お名前が入力されていません';
+  }
+  if ($key === 'furigana' && $rule === 'noempty') {
+    return 'フリガナが入力されていません';
+  }
+  if ($key === 'mail' && $rule === 'noempty') {
+    return 'E-mailが入力されていません';
+  }
+  if ($key === 'tell' && $rule === 'noempty') {
+    return '電話番号が入力されていません';
+  }
+  if ($key === 'contents' && $rule === 'noempty') {
+    return 'お問合せ内容が入力されていません';
+  }
+  return $error;
 }
 add_filter('mwform_error_message_mw-wp-form-5', 'my_error_message', 10, 3);
