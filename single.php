6f88703b3f1393129887cf9251b28db0131e875e
Taku <63911366+saeki-taku@users.noreply.github.com>
2021-06-11 23:00:02 +0900

    single-page fix

--- single.php
+++ single.php
@@ -6,9 +6,9 @@
 	<main class="main">
 		<article class="l-common p-work-detail">
 			<?php $title = get_the_title();?>
-			<?php  the_post_thumbnail(array(1200, 800) ,array('alt' => $title, 'class' => 'p-work-detail__siteimg')); ?>
 			<h2 class="p-work-detail__title"><?= $title?></h2>
 			<?php the_content(); ?>
+			
 		</article>
 	</main>
 	<?php get_template_part('inc/footer'); ?>
